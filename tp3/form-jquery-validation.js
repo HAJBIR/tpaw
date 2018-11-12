cd3d62e2e8c2f1790990d24693321ecf7115069a
HAJBIR <zakia.hajbir1995@gmail.com>
2018-11-12 15:39:59 +0100

    hg

---
+++ tp3/form-jquery-validation.js
@@ -0,0 +1,54 @@
+
+
+
+
+$( document ).ready(function() {
+    // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
+    // voir plus : https://www.w3schools.com/js/js_htmldom.asp
+     console.log( "DOM ready!" );
+
+    
+
+
+     $("#Valider").on("click",function(event)
+          {event.preventDefault();
+            
+           
+
+
+     if ($( "#Nom" ).val().length<5) { 
+
+        $( ".modal-body" ).text("Nom requis ");
+        $( "#myModal" ).model("show ");
+
+     }
+
+         if ($( "#Prenom" ).val().length<5) { 
+
+            $( ".modal-body" ).text("Nom requis ");
+            $( "#myModal" ).model("show ");}
+
+            if ($( "#Date" ).val().length<5) { 
+
+                $( ".modal-body" ).text("Nom requis ");
+                $( "#myModal" ).model("show ");}
+
+           if ($( "#Addresse" ).val().length<5) { 
+
+                $( ".modal-body" ).text("Nom requis ");
+                $( "#myModal" ).model("show ");}
+
+                if ($( "#Email" ).val().length<5) { 
+
+                    $( ".modal-body" ).text("Nom requis ");
+                    $( "#myModal" ).model("show ");}
+                   
+
+      
+
+
+   
+     
+     // Y mettre le code jQuery pour valider tous les champs du formulaire
+ });
+});
