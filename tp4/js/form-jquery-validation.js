7dbb7e00e244587a75405560c21966b380f2a6dc
HAJBIR <zakia.hajbir1995@gmail.com>
2018-12-17 10:40:22 +0100

    Update tp4/js/form-jquery-validation.js

--- tp4/js/form-jquery-validation.js
+++ tp4/js/form-jquery-validation.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 
 
 
-        if ($("#Nom").val().length < 5) {
+        if ($("#name").val().length < 5) {
             $(".modal-body").text("Nom requis");
 
 
@@ -13,27 +13,27 @@ $(document).ready(function () {
 
         }
 
-        else if ($("#Prenom").val().length < 5) {
+        else if ($("#firstname").val().length < 5) {
             $(".modal-body").text("Prenom requis");
 
 
             $("#myModal").modal("show");
         }
 
-        else if ($("#Date").val().length < 5) {
+        else if ($("#birth").val().length < 5) {
             $(".modal-body").text("Date requis");
 
             $("#myModal").modal("show");
         }
 
-        else if ($("#Adresse").val().length < 5) {
+        else if ($("#adresse").val().length < 5) {
             $(".modal-body").text("Adresse requis");
 
             $("#myModal").modal("show");
             <button onclick="getLocation()">GPS</button>
         }
 
-        else if ($("#Email").val().length < 5) {
+        else if ($("#mail").val().length < 5) {
             $(".modal-body").text("Email requis");
             $("#myModal").modal("show");
         }
@@ -42,10 +42,10 @@ $(document).ready(function () {
         else {
 
 
-            $(".modal-title").html("Bienvenue " + $("#Nom").val());
+            $(".modal-title").html("Bienvenue " + $("#name").val());
 
-            $(".modal-body").html(" vous etes né le  : " + $("#Date").val());
-            $(".modal-body").append("<img src = 'https://maps.googleapis.com/maps/api/staticmap?markers=" + $("#Adresse").val() +
+            $(".modal-body").html(" vous etes né le  : " + $("#birth").val());
+            $(".modal-body").append("<img src = 'https://maps.googleapis.com/maps/api/staticmap?markers=" + $("#adresse").val() +
                 "&zoom=10&size=400x300&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg' >");
             $('#myModal').modal("show");
 
